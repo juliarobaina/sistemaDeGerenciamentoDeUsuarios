5212feae58e92106dc0006b5fa4f7cb3ef8db982
Julia Robaina <47131530+juliarobaina@users.noreply.github.com>
2026-01-21 16:01:07 -0300

    Update conexaoBD.php

--- bd/conexaoBD.php
+++ bd/conexaoBD.php
@@ -3,7 +3,6 @@ $hostname = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'gerenciamento_de_usuarios';
-//revisar o die, ver outra maneira pra tratar o erro
 $conexao = mysqli_connect($hostname, $username, $password, $database);
 
 
